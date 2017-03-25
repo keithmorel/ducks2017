6b230b4eceef3fcd671ad1f1ab999d5a1c65f9bb
Robert Herley <rherley@stevens.edu>
2017-03-25 15:24:23 -0400

    updated android project

--- index.android.js
+++ index.android.js
@@ -1,53 +1,23 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
+// Default React Imports
 import React, { Component } from 'react';
 import {
   AppRegistry,
-  StyleSheet,
+  Image,
+  View,
   Text,
-  View
+  StyleSheet
 } from 'react-native';
+
+// Component inputs
+import TestCom from './components/TestCom/TestCom';
+import CompassCom from './components/CompassCom/CompassCom';
 
 export default class ducks2017 extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
+        <TestCom />
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
 
 AppRegistry.registerComponent('ducks2017', () => ducks2017);
