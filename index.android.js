8c3b4ec1c9fd2fff3a6eba432d8bbcb7796c45e1
Robert Herley <rherley@stevens.edu>
2017-03-26 12:51:18 -0400

    updated android code

--- index.android.js
+++ index.android.js
@@ -2,20 +2,34 @@
 import React, { Component } from 'react';
 import {
   AppRegistry,
-  Image,
-  View,
-  Text,
-  StyleSheet
+  Navigator
 } from 'react-native';
 
 // Component inputs
 import TestCom from './components/TestCom/TestCom';
-import CompassCom from './components/CompassCom/CompassCom';
+import ChatroomCom from './components/ChatroomCom/ChatroomCom';
+import StartCom from './components/StartCom/StartCom'
+const styles = require('./styles.js')
+const constants = styles.constants;
 
 export default class ducks2017 extends Component {
+
+  renderScene (route, navigator) {
+    switch(route.id) {
+      case 'StartCom':
+        return (<StartCom navigator={navigator} title="Home Screen" />)
+      case 'ChatroomCom':
+        return (<ChatroomCom navigator={navigator} title="Chat" />)
+    }
+  }
+
   render() {
     return (
-        <TestCom />
+      <Navigator
+        initialRoute={{id: 'StartCom'}}
+        renderScene={this.renderScene}
+        configureScreen={(route, routeStack) => Navigator.SceneConfigs.FloatFromBottom}
+      />
     );
   }
 }
